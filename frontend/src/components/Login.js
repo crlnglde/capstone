ce7f5e9760aa93e1cd38834426165560e1f5f8c5
Carl Angelo D. Elevera <carlangelo.elevera@g.msuiit.edu.ph>
2025-03-20 10:23:03 +0800

    feat:

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "../css/Login.css"
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { motion } from "framer-motion";
+import CSWD from "../pic/cswdlogo.png"
+
+const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+      };
+
+    return (
+      <div className="container">
+        <div className="loginBox">
+          <div className="leftPanel">
+            <h2 className="title">Log In</h2>
+            <p className="subtitle">Please enter your details</p>
+            <form className="form">
+                <input type="email" placeholder="Email" className="input" />
+                <div className="passwordContainer">
+                    <input 
+                        type={showPassword ? "text" : "password"} 
+                        placeholder="Password" 
+                        className="input"
+                    />
+                    <span className="eyeIcon" onClick={togglePasswordVisibility}>
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </span>
+                </div>
+                <div className="forgotPassword">forgot password?</div>
+                <button className="loginButton">Log in</button>
+            </form>
+            
+          </div>
+          <motion.div 
+            className="rightPanel"
+            initial={{ opacity: 0, x: 50 }} // Start hidden and slightly right
+            animate={{ opacity: 1, x: 0 }} // Fade in and move to position
+            transition={{ duration: 1, ease: "easeOut" }} // Smooth transition
+            >
+            <img src={CSWD} alt="Fitness" className="image" />
+            </motion.div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Login;
+  
